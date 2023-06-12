9df6a8ae0adec8b4d341f23bed472af92a5da83d
Vicente <vicentevillarreal123@gmail.com>
2023-06-12 13:29:29 +0200

    commit

--- src/app/books/list-by-cat/list-by-cat.component.ts
+++ src/app/books/list-by-cat/list-by-cat.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BooksService } from '../services/books.service';
 import { Books } from '../../interfaces/bookInterface';
 import Swal from 'sweetalert2';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { ShoppingCartService } from 'src/app/shopping-cart/shopping-cart.service';
 
 @Component({
   selector: 'app-list-by-cat',
@@ -12,8 +14,13 @@ import Swal from 'sweetalert2';
 export class ListByCatComponent {
 
   public books :Books[]=[]
-  
-  constructor(private route : ActivatedRoute, private servicio:BooksService ) { }
+  isAdmin=false;
+  jwt: string | null = null;
+  username=localStorage.getItem("username")
+  addedFav=false;
+
+
+  constructor(private route : ActivatedRoute, private servicio:BooksService, private router:Router, private authSer: AuthService, private shopping : ShoppingCartService) { }
 
 
 // Lógica sencilla, solo recuperamos el parámetro y obtenemos los libros de una categoría.
@@ -23,10 +30,51 @@ export class ListByCatComponent {
     this.servicio.getBooksByCat(id).subscribe({
       next:(resp)=> {
         this.books=resp
+        this.jwt = localStorage.getItem('jwt');
+  
+   
+   if(this.jwt){
+     this.isAdmin = this.authSer.isUserAdmin(this.jwt);
+   }
       },error:(err)=> {
         
       },
     })
+
+  }
+
+  addToCart(book:Books){
+    this.shopping.agregarAlCarrito(book)
+    window.sessionStorage.setItem('carrito', JSON.stringify(this.shopping.books));
+  }
+
+  addToFavorite(book:Books){
+    this.username= localStorage.getItem("username")
+    if(this.username!=null){
+      this.servicio.addToFavoriteBook(book, this.username).subscribe({
+        next:(resp) =>{
+          this.addedFav=true;
+           
+              Swal.fire({
+                icon: 'success',
+                title: 'Favorite add',
+                text: '¡Favorite added!',
+            });
+            this.router.navigate(['/favorites/list'])
+           
+        
+        }, error:(err)=> {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Favorito ya esta añadido!',
+          })
+          this.router.navigate(['/favorites/list'])
+        },
+      })
+
+    }
+   
 
   }
 
